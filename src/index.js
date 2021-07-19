450714b3c06ae7ff2dc48623fa44708bee44f12f
ansgmailgithub <33917532+ansgmailgithub@users.noreply.github.com>
2021-07-18 22:35:26 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// var http = require("http");
+
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
+
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("abcd!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
